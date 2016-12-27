e629937b9fef9d0a43dad60a0e3245a3f6561eb4
Cindy Sun <c.s.sun5@gmail.com>
2016-12-26 16:33:23 -0800

    chore(init): fix and remove dependences in package.json

--- server/server.js
+++ server/server.js
@@ -14,62 +14,57 @@ module.exports = app;
 var dataServer = new Promise(add_restful_models(app));
 
 if (!module.parent) {
-  dataServer.then(function(srv) {
-    start_app(srv);
-  });
+    dataServer.then(function(srv) {
+        start_app(srv);
+    });
 }
 
 function add_restful_models(app) {
-  return insert_restful_models;
+    return insert_restful_models;
 
-  function insert_restful_models(resolve, reject) {
-    var restful = new Promise(models.restify_cruds);
-    restful.then(function(restful_api) {
-      app.use('/api', restful_api);
-      resolve(app);
-    });
-    return restful;
-  }
+    function insert_restful_models(resolve, reject) {
+        var restful = new Promise(models.restify_cruds);
+        restful.then(function(restful_api) {
+            app.use('/api', restful_api);
+            resolve(app);
+        });
+        return restful;
+    }
 }
 
 function create_app(app) {
 
-  app.set('port', config.server.port);
+    app.set('port', config.server.port);
 
-  app.use(morgan('combined'));
-  app.use(express.static(path.join(__dirname, '../public')));
-  app.use(express.static(path.join(__dirname, '../bower_components')));
-  app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-  app.use(bodyParser.json({limit: '10mb'}));
+    app.use(morgan('combined'));
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.use(express.static(path.join(__dirname, '../bower_components')));
+    app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+    app.use(bodyParser.json({ limit: '10mb' }));
 
-// var upload = multer({ dest: './uploads/' });
-//  app.post('/upload', upload.single('file'), function(req,res,next){
-//     console.log('Uploade Successful ', req.file, req.body);
-// });
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+    //Route to index page regardless of what url is
+    // app.get('/*', function(req, res){
+    //   res.sendFile(path.join(__dirname, '../public/index.html'));
+    // });
 
-//Route to index page regardless of what url is
-  // app.get('/*', function(req, res){
-  //   res.sendFile(path.join(__dirname, '../public/index.html'));
-  // });
-
-  return app;
+    return app;
 }
 
 function start_app(app) {
-  var port = app.get('port');
-  var server = app.listen(app.get('port'), wakeUp);
-  return server;
+    var port = app.get('port');
+    var server = app.listen(app.get('port'), wakeUp);
+    return server;
 
-  function wakeUp() {
-    var address = server.address().address;
-    if (server.address().address === '::') {
-      address = 'localhost';
+    function wakeUp() {
+        var address = server.address().address;
+        if (server.address().address === '::') {
+            address = 'localhost';
+        }
+        address += ':' + server.address().port;
+        console.log(chalk.green('OK'), chalk.yellow('listen'), 'node server', chalk.blue(address));
     }
-    address += ':' + server.address().port;
-    console.log(chalk.green('OK'), chalk.yellow('listen'), 'node server', chalk.blue(address));
-  }
 }
